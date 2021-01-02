e23503a91971dbb64d6edcf5aa959f320ad77883
xsharonhe <xsharonhe@gmail.com>
2021-01-01 22:46:19 -0500

    Preilminary rest api and cors added

--- backend/sentimentanalyzer/settings.py
+++ backend/sentimentanalyzer/settings.py
@@ -41,10 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheader',
     'datacollector.apps.DatacollectorConfig'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.jangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+     'localhost:3000/'
+ )
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
